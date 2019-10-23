f43b33f4ec290a140259df84fcc27a4aee98bb6c
John Baldo <1191015+jbaldo@users.noreply.github.com>
2019-10-22 19:38:15 -0700

    add sass, bootstrap, and proof that both are working

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,15 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import "../styles/style.scss"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1 class="hello">Hi people</h1>
+    <div class="alert alert-success" role="alert">
+      A simple success alert—check it out!
+    </div>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
