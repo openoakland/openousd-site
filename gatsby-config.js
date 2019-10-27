6953e654de05794a93514aacd6e4324b1910d095
John Baldo <1191015+jbaldo@users.noreply.github.com>
2019-10-27 15:51:27 -0700

    add bootstrap and react-bootstrap as deps, move style sheet

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `OpenOUSD`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@OpenOakland`,
   },
   plugins: [
     `gatsby-plugin-sass`,
