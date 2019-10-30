1e7b6609fd1c2e8e332d684f32486dd87ced375c
John Baldo <1191015+jbaldo@users.noreply.github.com>
2019-10-29 21:43:46 -0700

    not really working nav, bootstrap cols

--- src/components/header.js
+++ src/components/header.js
@@ -3,29 +3,41 @@ import PropTypes from "prop-types"
 import React from "react"
 
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 import "./header.scss"
+import logo from "../images/oo-logo-color.svg"
 
 
 const Header = ({ siteTitle }) => (
   <header>
-  <Container>
-    <Navbar expand="md" fixed="top">
-      <Navbar.Brand>
-        <Link to="/"> {siteTitle} </Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="menu justify-content-center">
-          <Nav.Item>Home</Nav.Item>
-          <Nav.Item>Departments</Nav.Item>
-          <Nav.Item><Link to="/page-2/">Contact</Link></Nav.Item>
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar expand="md" bg="white" variant="light" fixed="top">
+      <Container>
+        <Row>
+          <Col sm={12} lg={6}>
+            <Link to="/"><img id="logo" src={logo} alt="Logo" /></Link>
+          </Col>
+          <Col sm={12} lg={6}>
+            <div>Volunteers providing transparent access to Oakland Unified School District budget & data</div>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="menu justify-content-center">
+                <Nav.Item>Home</Nav.Item>
+                <Nav.Item>Departments</Nav.Item>
+                <Nav.Item><Link to="/page-2/">Contact</Link></Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Col>
+        </Row>
+      </Container>
     </Navbar>
-    </Container>
     <div
       style={{
         margin: `0 auto`,
