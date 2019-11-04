4f9712778683df2b92da6a21f7d64f96497f298b
John Baldo <1191015+jbaldo@users.noreply.github.com>
2019-11-03 19:14:49 -0800

    nav almost there, needs menu fix

--- src/components/header.js
+++ src/components/header.js
@@ -12,21 +12,25 @@ import logo from "../images/oo-logo-combined.svg"
 
 const Header = ({ siteTitle }) => (
     <header>
-        <Navbar bg="white" variant="light" fixed="top" expand="md">
-            <div className="mx-sm-auto">
-                <Link to="/"><img id="logo" src={logo} alt="Logo" /></Link>
-            </div>
-            <Navbar.Text >
-                <div className="d-none d-lg-block">Volunteers providing transparent access to Oakland Unified School District budget & data</div>
-            </Navbar.Text>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav" className="flex-grow-1 text-right">
-                <Nav className="menu ml-auto flex-nowrap">
-                    <Nav.Item>Home</Nav.Item>
-                    <Nav.Item>Departments</Nav.Item>
-                    <Nav.Item><Link to="/page-2/">Contact</Link></Nav.Item>
-                </Nav>
-            </Navbar.Collapse>
+        <Navbar bg="white" variant="light" fixed="top" expand="md"  className="d-md-flex flex-column">
+                <div id="row1" className="d-flex flex-row align-items-center">
+                    <div id="logo" className="mx-auto ml-md-4">
+                        <Link to="/"><img src={logo} alt="Logo" /></Link>
+                    </div>
+                    <div id="tagline" className="d-none d-lg-block">
+                        Volunteers providing transparent access to Oakland Unified School District budget & data
+                    </div>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                </div>
+                <div id="row2">
+                    <Navbar.Collapse id="basic-navbar-nav" className="flex-grow-1 text-right">
+                        <Nav className="menu flex-nowrap">
+                            <Nav.Item>Home</Nav.Item>
+                            <Nav.Item>Departments</Nav.Item>
+                            <Nav.Item><Link to="/page-2/">Contact</Link></Nav.Item>
+                        </Nav>
+                    </Navbar.Collapse>
+                </div>
         </Navbar>
     </header>
 )
