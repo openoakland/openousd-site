4864f766a316262e89a4affe92a1be31aca2a12b
John Baldo <1191015+jbaldo@users.noreply.github.com>
2019-11-24 20:15:22 -0800

    departments json fetch

---
+++ scripts/build_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import json
+import requests
+import os
+
+api_location = "https://us-central1-openousd.cloudfunctions.net/openousd"
+base_path = api_location + "/api"
+
+data_path = os.path.join(os.path.dirname(__file__), "../src/data/")
+
+# Get department data
+response = requests.get(base_path + "/departments")
+f = open(data_path + "departments.json","w+")
+# f.write(response.json())
+json.dump(response.json(),f)
+f.close()
