5f96876ddf8b0216da6d5f4c189de8f9699663eb
John Baldo <1191015+jbaldo@users.noreply.github.com>
2020-01-05 12:25:52 -0800

    use categories and add currency formatting

--- src/components/sankey-chart.js
+++ src/components/sankey-chart.js
@@ -10,13 +10,14 @@ import { ResponsiveSankey } from '@nivo/sankey'
 const Sankey = ({ data /* see data tab */ }) => (
     <ResponsiveSankey
         data={data}
-        margin={{ top: 40, right: 100, bottom: 40, left: 50 }}
+        margin={{ top: 20, right: 200, bottom: 20, left: 240 }}
+        sort="auto"
         align="justify"
         colors={{ scheme: 'category10' }}
         nodeOpacity={1}
         nodeThickness={25}
         nodeInnerPadding={3}
-        nodeSpacing={24}
+        nodeSpacing={10}
         nodeBorderWidth={0}
         nodeBorderColor={{ from: 'color', modifiers: [ [ 'darker', 0.8 ] ] }}
         linkOpacity={0.2}
@@ -26,9 +27,16 @@ const Sankey = ({ data /* see data tab */ }) => (
         labelOrientation="horizontal"
         labelPadding={16}
         labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1 ] ] }}
-        animate={true}
+        animate={false}
         motionStiffness={140}
         motionDamping={13}
+        tooltipFormat={value =>
+            `${Math.floor(Number(value)).toLocaleString("en-US",
+                {style: "currency",
+                currency: "USD",
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0})}`
+        }
     />
 )
 export default Sankey
--- src/pages/departments.js
+++ src/pages/departments.js
@@ -5,85 +5,11 @@ import SEO from "../components/seo"
 import Table from "../components/departments-table"
 import Sankey from "../components/sankey-chart"
 import { graphql } from "gatsby"
+import sankeyData from "../../data/sankey.json"
 
 import "../styles/pages/departments.scss"
 
 const DepartmentsPage = ({ data }) => {
-    const sankeyData = {
-    "nodes": [
-      {
-        "id": "John",
-        "color": "hsl(156, 70%, 50%)"
-      },
-      {
-        "id": "Raoul",
-        "color": "hsl(337, 70%, 50%)"
-      },
-      {
-        "id": "Jane",
-        "color": "hsl(12, 70%, 50%)"
-      },
-      {
-        "id": "Marcel",
-        "color": "hsl(25, 70%, 50%)"
-      },
-      {
-        "id": "Ibrahim",
-        "color": "hsl(292, 70%, 50%)"
-      },
-      {
-        "id": "Junko",
-        "color": "hsl(221, 70%, 50%)"
-      }
-    ],
-    "links": [
-      {
-        "source": "Jane",
-        "target": "Raoul",
-        "value": 170
-      },
-      {
-        "source": "Jane",
-        "target": "Junko",
-        "value": 189
-      },
-      {
-        "source": "Jane",
-        "target": "Marcel",
-        "value": 102
-      },
-      {
-        "source": "Raoul",
-        "target": "Ibrahim",
-        "value": 32
-      },
-      {
-        "source": "Raoul",
-        "target": "John",
-        "value": 50
-      },
-      {
-        "source": "John",
-        "target": "Marcel",
-        "value": 169
-      },
-      {
-        "source": "John",
-        "target": "Ibrahim",
-        "value": 183
-      },
-      {
-        "source": "John",
-        "target": "Junko",
-        "value": 143
-      },
-      {
-        "source": "Marcel",
-        "target": "Ibrahim",
-        "value": 149
-      }
-    ]
-  }
 
   const departments = data.allDepartmentsJson.nodes;
   return (
