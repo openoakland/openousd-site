f54e09009503f2a1254e71cafc2ef5b260c421d4
John Baldo <1191015+jbaldo@users.noreply.github.com>
2020-01-28 20:00:36 -0800

    compiled site

--- README.md
+++
@@ -1,5 +0,0 @@
-### Overview
-
-The goal of this project is to explain to school communities what the Oakland Unified School District’s central office does and help them weigh in on what its function should be in the future. Let’s reimagine what OUSD district office can look like!
-
-We welcome new team members, especially if they are skilled in Gatsby.js, GraphQL, or PostgreSQL (or are willing to learn).
---
+++ component---src-pages-404-js-4a7e1d4505e81123081f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{245:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(243),o=n(242);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-4a7e1d4505e81123081f.js.map
