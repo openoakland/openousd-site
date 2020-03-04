487ff0a5f187a9903f9e77f844ad821a9d94a944
John Baldo <1191015+jbaldo@users.noreply.github.com>
2020-03-03 19:14:56 -0800

    front page copy adjustment

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,13 +20,13 @@ const IndexPage = () => (
         <Row className="descriptions justify-content-center mx-0">
           <Col xs={11} md={5} className="px-md-5 py-md-4 px-3 py-3">
             <h1 className="">What is OpenOUSD?</h1>
-            <p>OpenOUSD is a project created out of OpenOakland. It’s a non-profit, volunteer group with the mission of bridging technology and community. OpenOUSD aims to support Oakland schools by providing transparent budget data of the OUSD central office, enabling the community to parcipate in informed conversations about the budget.</p>
+            <p>OpenOUSD is a project created out of OpenOakland, a volunteer run group with the mission of increasing access to government through technology. OpenOUSD aims to bring greater transparency to the Oakland Unified School District's central office so that the community can better participate in the discussions about how it can best serve our schools.</p>
           </Col>
 
           <Col xs={11} md={5} className="px-md-5 py-md-4 px-3 py-3">
-            <h1 className="">What is the OUSD Central Office?</h1>
-            <p>The central office is an Oakland city organization comprised of almost 50 different departments whose purposes are to support the 121 schools and 50,000 students within the district of Oakland. </p>
-            <Link to="/central-programs/"><Button variant="primary" size="lg" className="cta">Explore the Departments</Button></Link>
+            <h1 className="">What are central programs?</h1>
+            <p>We define a central program as any activity managed by OUSD's central office rather than individual school sites. For example, a staff member working at a school site but hired by the central office would be considered part of a central program. There are more than 50 centrally managed programs at OUSD.</p>
+            <Link to="/central-programs/"><Button variant="primary" size="lg" className="cta">Explore OUSD's central programs</Button></Link>
           </Col>
         </Row>
     </Container>
