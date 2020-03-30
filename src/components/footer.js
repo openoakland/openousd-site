8e3eeccc54185e5d3fc0f6a65a47c901e35336a0
John Baldo <1191015+jbaldo@users.noreply.github.com>
2020-03-29 20:15:47 -0700

    take email address out of footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 import "./footer.scss"
 import logo from '../images/logo_open-oakland_white.svg'
@@ -7,7 +8,7 @@ import logo from '../images/logo_open-oakland_white.svg'
 const Footer = () => (
     <footer className="footer">
         <p><strong>Created by</strong> <img id="footerlogo" src={logo} alt="OpenOakland"></img></p>
-        <p>Disclaimer: This site is in beta. Please report bugs and suggestions to ousdbudget@openoakland.org</p>
+        <p>Disclaimer: This site is in beta. Please report bugs and share feedback on <Link to="/contact">our contact page</Link></p>
 
         <p>© {new Date().getFullYear()}</p>
 
