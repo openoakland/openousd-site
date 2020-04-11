6892f0b29e3f950026b7ebe129c1e856c5bc6130
John Baldo <1191015+jbaldo@users.noreply.github.com>
2020-04-11 15:51:29 -0700

    fixing branch checkout in promotion script

--- scripts/promote_staging.sh
+++ scripts/promote_staging.sh
@@ -1,5 +1,5 @@
 # store starting branch so we can return to it
-STARTING_BRANCH="$(git branch)"
+STARTING_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 # fetch latest from git
 git fetch
@@ -21,4 +21,5 @@ gh-pages -d . \
     -m "staging promoted at commit $(git rev-parse --short HEAD) $(date +%m-%d-%Y_%H:%M:%S)" \
     -r git@github.com:openoakland/openousd-site-prod.git
 
+echo "returning to ${STARTING_BRANCH} branch"
 git checkout ${STARTING_BRANCH}
