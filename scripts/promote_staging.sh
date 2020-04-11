be148ece9a98779d4a478e107a7f150e75d40fce
John Baldo <1191015+jbaldo@users.noreply.github.com>
2020-04-11 14:45:33 -0700

    adding console output to scrips

--- scripts/promote_staging.sh
+++ scripts/promote_staging.sh
@@ -7,13 +7,15 @@ git fetch
 # checkout the staging branch
 git checkout origin/gh-pages
 
-# remove the staging.openousd.org domain
+echo "removing the staging.openousd.org domain"
 rm CNAME
 
 # create a CNAME for production
+echo "creating new CNAME"
 echo 'openousd.org' > 'CNAME'
 
 # push it to the production hosting repo
+echo "running gh-pages"
 gh-pages -b master \
     -m "staging promoted at commit $(git rev-parse --short HEAD) $(date +%m-%d-%Y_%H:%M:%S)" \
     -r git@github.com:openoakland/openousd-site-prod.git
