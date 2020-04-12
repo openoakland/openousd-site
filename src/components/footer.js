9d9129666aa522e663df401212bf26271555b7b1
John Baldo <1191015+jbaldo@users.noreply.github.com>
2020-04-11 19:29:25 -0700

    fix link in the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,8 @@ import logo from '../images/logo_open-oakland_white.svg'
 const Footer = () => (
     <footer className="footer">
         <p><strong>Created by</strong> <img id="footerlogo" src={logo} alt="OpenOakland logo"></img></p>
-        <p className="py-3">OpenOUSD is an ongoing project. Please report problems and share feedback on the <Link to="/contact">contact page</Link></p>
+        <p className="py-3">OpenOUSD is an ongoing project.{' '}
+        Please report problems and share feedback on the <Link to="/contact/">contact page</Link></p>
 
         <p>© {new Date().getFullYear()}</p>
 
