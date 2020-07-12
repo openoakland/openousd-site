0e03c822e58bb6185804dc70599c257e3e27e453
Vanessa <vahurtad@gmail.com>
2020-07-11 23:31:51 -0700

    changes to css for whats-new page and scrolling

--- src/pages/whats-new.js
+++ src/pages/whats-new.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
 import { Container, Row, Col, ListGroup } from 'react-bootstrap';
+import { Link, Element } from "react-scroll";
 
 import twitterIcon from '../images/icons/twitter-icon-blue.svg'
 import NewFeature from "../components/new-feature"
@@ -10,38 +10,51 @@ import SEO from "../components/seo"
 import newFeaturesData from "../../data/new-features.json"
 import image from "../images/feature-june.gif"
 
+import "../styles/pages/whats-new.scss"
+
+const dateToDivID = (date) => {return "#" + date.replace(/[\W_]+/g, '-').toLowerCase()}
+
 const WhatsNew = () => (
   <Layout pageClassName="whats-new-page">
     <SEO title="What's New" />
     <Container>
     {/*<Row><Col><h2>What's New</h2></Col></Row>*/}
         <Row>
-            <Col md={4}>
+            <Col md={4} id="left-dates">
                 <ListGroup variant="flush" className="mt-3">
                 {
                     newFeaturesData.map(feature => (
-                        <ListGroup.Item action>
-                            {feature.date}
+                        <ListGroup.Item action >
+                            <Link
+                            activeClass="active"
+                            spy={true}
+                            smooth={true}
+                            offset={-150}
+                            duration= {550} 
+                            to={dateToDivID(feature.date)}>
+                                {feature.date}
+                            </Link>
                         </ListGroup.Item>
                     ))
                 }
                 </ListGroup>
             </Col>
-            <Col md={8}>
+            <Col md={8} id="right-content">
                 {
                     newFeaturesData.map(feature => (
-                        <NewFeature
-                            heading={feature.heading}
-                            date={feature.date}
-                            description={feature.description}
-                            path={feature.page_path}
-                            image={image}
-                        />
+                        <Element name={dateToDivID(feature.date)}  style={{marginBottom: '80px'}}>
+                            <NewFeature
+                                heading={feature.heading}
+                                date={feature.date}
+                                description={feature.description}
+                                path={feature.page_path}
+                                image={image}
+                            />
+                        </Element>
                     ))}
             </Col>
         </Row>
     </Container>
-
   </Layout>
 )
 
