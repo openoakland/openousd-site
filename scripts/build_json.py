f150039047cc15841b138044a366b59a6c1b9795
jbaldo <1191015+jbaldo@users.noreply.github.com>
2020-10-04 16:32:10 -0700

    pull data from local db, should be the same but different order (#38)

--- scripts/build_json.py
+++ scripts/build_json.py
@@ -4,7 +4,9 @@ import json
 import requests
 import os
 
-api_location = "https://us-central1-openousd.cloudfunctions.net/openousd"
+# hosted GCloud functions no longer have a hosted db connection
+# api_location = "https://us-central1-openousd.cloudfunctions.net/openousd"
+api_location = "http://localhost:8080"
 base_path = api_location + "/api"
 
 data_path = os.path.join(os.path.dirname(__file__), "../data/")
@@ -27,4 +29,9 @@ with open(data_path + "sankey.json","w+") as f:
 # Get sankey data (restricted / unrestricted grouping)
 response = requests.get(base_path + "/sankey?year=2018&groupBy=restricted")
 with open(data_path + "sankey-restricted.json","w+") as f:
+    json.dump(response.json(),f, indent=2)
+
+# Get sankey data for each central program
+response = requests.get(base_path + "/central-programs/sankey?year=2018")
+with open(data_path + "central-programs-sankey.json","w+") as f:
     json.dump(response.json(),f, indent=2)
