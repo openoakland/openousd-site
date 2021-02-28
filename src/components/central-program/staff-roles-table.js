0dd7ab50f00cf19a3d6c0f1faebefd8e876d97d6
John Baldo <1191015+jbaldo@users.noreply.github.com>
2021-02-28 15:13:07 -0800

    use total label in the table

--- src/components/central-program/staff-roles-table.js
+++ src/components/central-program/staff-roles-table.js
@@ -6,34 +6,9 @@ import ToolkitProvider from "react-bootstrap-table2-toolkit"
 import { getSortCaret, sort, rowUnderline } from "../table-utilities"
 import { getColumnsByDataField } from "../../utilities/content-utilities"
 
-const TOTAL_ROW_NAME = "Total"
 let columnsByDataField
 const ROLE_DESCRIPTION = "role_description"
 const ROLE_TOTAL_POSITIONS = "eoy_total_positions_for_role"
-
-const createFirstRow = data => {
-  const initialObject = {
-    [ROLE_DESCRIPTION]: TOTAL_ROW_NAME,
-    [ROLE_TOTAL_POSITIONS]: 0,
-  }
-  return data.reduce((returnObject, currentItem) => {
-    returnObject[ROLE_TOTAL_POSITIONS] += +currentItem[ROLE_TOTAL_POSITIONS]
-    return returnObject
-  }, initialObject)
-}
-
-const sortStaffRoles = (a, b, order, dataField, rowA, rowB) => {
-  return sort(
-    a,
-    b,
-    order,
-    dataField,
-    rowA,
-    rowB,
-    ROLE_DESCRIPTION,
-    TOTAL_ROW_NAME
-  )
-}
 
 const getColumns = () => [
   {
@@ -72,11 +47,36 @@ const getColumns = () => [
 ]
 
 const StaffRolesTable = ({ data, content }) => {
+  const TOTAL_ROW_NAME = content.labels.totalLabel
   columnsByDataField = getColumnsByDataField(content.columns)
   const columns = getColumns()
 
   const firstRow = createFirstRow(data)
   data = data.concat([firstRow])
+
+  const createFirstRow = data => {
+    const initialObject = {
+      [ROLE_DESCRIPTION]: TOTAL_ROW_NAME,
+      [ROLE_TOTAL_POSITIONS]: 0,
+    }
+    return data.reduce((returnObject, currentItem) => {
+      returnObject[ROLE_TOTAL_POSITIONS] += +currentItem[ROLE_TOTAL_POSITIONS]
+      return returnObject
+    }, initialObject)
+  }
+
+  const sortStaffRoles = (a, b, order, dataField, rowA, rowB) => {
+    return sort(
+      a,
+      b,
+      order,
+      dataField,
+      rowA,
+      rowB,
+      ROLE_DESCRIPTION,
+      TOTAL_ROW_NAME
+    )
+  }
 
   return (
     <ToolkitProvider
