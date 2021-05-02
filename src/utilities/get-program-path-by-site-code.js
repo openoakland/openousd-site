72a0286f2fe7cf649b428dc6279982aa04de7dfa
John Baldo <1191015+jbaldo@users.noreply.github.com>
2021-05-02 16:40:05 -0700

    use spaces instead of tabs

--- src/utilities/get-program-path-by-site-code.js
+++ src/utilities/get-program-path-by-site-code.js
@@ -1,27 +1,27 @@
 import { useStaticQuery, graphql } from "gatsby"
 
 export const useGetProgramPathBySiteCode = () => {
-	const allCentralProgramNodes = useStaticQuery(
-		graphql`
-			query {
-				allCentralProgramsJson {
-					nodes {
-						code
-						fields {
-							path
-						}
-					}
-				}
-			}
-		`
-	)
-	let pathsBySiteCode = {}
+  const allCentralProgramNodes = useStaticQuery(
+    graphql`
+      query {
+        allCentralProgramsJson {
+          nodes {
+            code
+            fields {
+              path
+            }
+          }
+        }
+      }
+    `
+  )
+  let pathsBySiteCode = {}
 
-	allCentralProgramNodes.allCentralProgramsJson.nodes.forEach(program => {
-		pathsBySiteCode[program.code] = program.fields.path
-	})
+  allCentralProgramNodes.allCentralProgramsJson.nodes.forEach(program => {
+    pathsBySiteCode[program.code] = program.fields.path
+  })
 
-	return siteCode => {
-		return pathsBySiteCode[siteCode]
-	}
+  return siteCode => {
+    return pathsBySiteCode[siteCode]
+  }
 }
