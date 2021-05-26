e9c792dc2db3262b908a4533d48222162b015f94
jbaldo <1191015+jbaldo@users.noreply.github.com>
2021-05-25 20:25:25 -0700

    Fix gatsby-plugin-intl build error (#78)
    * add back files to fix compile
    
    * put dummy content in translation files
    
    * rename directory so it doesn't suggest that there are translations in it

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,82 +8,79 @@ require("dotenv").config({
 })
 
 module.exports = {
-    pathPrefix: "/openousd-site",
-    siteMetadata: {
-        title: `OpenOUSD`,
-        description: `OpenOUSD aims to bring greater transparency to the Oakland Unified School District's central office.`,
-        author: `@OpenOUSD`,
-        latestSchoolYear: `2019-20`
+  pathPrefix: "/openousd-site",
+  siteMetadata: {
+    title: `OpenOUSD`,
+    description: `OpenOUSD aims to bring greater transparency to the Oakland Unified School District's central office.`,
+    author: `@OpenOUSD`,
+    latestSchoolYear: `2019-20`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-69150718-3",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+      },
     },
-    plugins: [
-        {
-            resolve: `gatsby-plugin-google-analytics`,
-            options: {
-                trackingId: "UA-69150718-3",
-                // Defines where to place the tracking script - `true` in the head and `false` in the body
-                head: false,
-            },
-        },
-        {
-          resolve: 'gatsby-plugin-sass',
-          options: {
-            data: `@import "./src/styles/_variables.scss";`,
-          }
-        },
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                path: `${__dirname}/data/`,
-            },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-intl`,
-            options: {
-                path: `${__dirname}/src/translations`,
-                languages: [`en`, `es`],
-                defaultLanguage: `en`,
-                redirect: true,
-            },
-        },
-        {
-          resolve: `gatsby-source-contentful`,
-          options: {
-            spaceId: `tqd0xcamk1ij`,
-            accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-          },
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `OpenOUSD`,
-                short_name: `OpenOUSD`,
-                start_url: `/`,
-                background_color: `#0042E6`,
-                theme_color: `#0042E6`,
-                display: `browser`,
-                icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-            }
-        },
-        {
-            resolve: `gatsby-plugin-google-fonts`,
-            options: {
-                fonts: [
-                    `Nunito Sans\:400,400i,800,900`,
-                    `Roboto Mono\:400,700`
-                ],
-                display: 'swap'
-            }
-        },
-        "gatsby-transformer-json",
-    ],
+    {
+      resolve: "gatsby-plugin-sass",
+      options: {
+        data: `@import "./src/styles/_variables.scss";`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/data/`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-intl`,
+      options: {
+        path: `${__dirname}/src/languages`,
+        languages: [`en`, `es`],
+        defaultLanguage: `en`,
+        redirect: true,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `tqd0xcamk1ij`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `OpenOUSD`,
+        short_name: `OpenOUSD`,
+        start_url: `/`,
+        background_color: `#0042E6`,
+        theme_color: `#0042E6`,
+        display: `browser`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Nunito Sans\:400,400i,800,900`, `Roboto Mono\:400,700`],
+        display: "swap",
+      },
+    },
+    "gatsby-transformer-json",
+  ],
 }
